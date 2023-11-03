282888dcd82a6f8431e248ae95031042b68c3b39
baynobu <darmawanhanif30@gmail.com>
2023-11-03 13:30:28 +0700

    update: validasi reg

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -18,22 +18,32 @@ export const Login = async (req, res) =>{
     res.status(200).json({uuid, name, email, role});
 }
 
-export const Register = async(req, res) =>{
-    const {name, email, password, confPassword, nik_nis} = req.body;
-    if(password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok"});
-    const hashPassword = await argon2.hash(password);
-    try {
-        await User.create({
-            name: name,
-            email: email,
-            password: hashPassword,
-            nik_nis: nik_nis
-        });
-        res.status(201).json({msg: "Register Berhasil"});
-    } catch (error) {
-        res.status(400).json({msg: error.message});
+export const Register = async (req, res) => {
+    const { name, email, password, confPassword, nik_nis } = req.body;
+  
+    // Validasi email dan nik_nis di database
+    const existingUser = await User.findOne({ $or: [{ email: email }, { nik_nis: nik_nis }] });
+    if (existingUser) {
+      return res.status(400).json({ msg: "Email atau NIK/NIS sudah terdaftar." });
     }
-}
+  
+    if (password !== confPassword) return res.status(400).json({ msg: "Password dan Confirm Password tidak cocok" });
+  
+    const hashPassword = await argon2.hash(password);
+  
+    try {
+      await User.create({
+        name: name,
+        email: email,
+        password: hashPassword,
+        nik_nis: nik_nis
+      });
+      res.status(201).json({ msg: "Register Berhasil" });
+    } catch (error) {
+      res.status(400).json({ msg: error.message });
+    }
+  };
+  
 
 export const logOut = (req, res) =>{
     req.session.destroy((err)=>{
