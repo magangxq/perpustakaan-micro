a421d1078ecea99b02a5852fe0f0cb86821f657a
rafli putra <rafliputra634@gmail.com>
2023-11-15 16:12:56 +0700

    addition picture and dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import logo from "../logo.png";
+import logo from "../image/logo.png";
 import { useDispatch } from "react-redux";
 import { LogOut, reset } from "../features/authSlice";
+import { IoCaretDown } from "react-icons/io5";
+import userImage from "../image/user.jpg";
+import axios from "axios";
 
 const Navbar = () => {
+  const [imageSrc, setImageSrc] = useState(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    getPicture();
+  }, []);
+
+  const getPicture = async () => {
+    axios.get("http://localhost:2000/user/profile")
+      .then(response => {
+        if (response.data && response.data.picture) {
+          // console.log(response.data.picture)
+          const imageUrl = response.data.picture;
+          setImageSrc(imageUrl)
+        } else {
+          // console.log('Data picture tidak ditemukan dalam response.')
+          setImageSrc(false)
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+  }
 
   const logout = () => {
     dispatch(LogOut());
@@ -30,11 +55,39 @@ const Navbar = () => {
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-end">
             <div className="navbar-item">
-              <div className="buttons">
+              <div className="dropdown is-hoverable">
+                <div className="dropdown-trigger">
+                  <div className="is-flex is-justify-content-center is-align-items-center" aria-haspopup="true" aria-controls="dropdown-menu3" style={{ width: "60px" }}>
+                    <figure className="image is-24x24 ">
+                      {imageSrc === false ? (
+                        <img className="is-rounded" src={userImage} alt="user profile" />
+                      ) : (
+                        <img className="is-rounded" src={imageSrc} alt="user profile" />
+                      )}
+                    </figure>
+                    <span className="icon is-small">
+                      <IoCaretDown />
+                    </span>
+                  </div>
+                </div>
+                <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+                  <div className="dropdown-content">
+                    <a href="/profile" className="dropdown-item">
+                      Profile
+                    </a>
+                    <span onClick={logout} className="dropdown-item cur" style={{ cursor: "pointer" }}>
+                      Logout
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+
+              {/* <div className="buttons">
                 <button onClick={logout} className="button is-light">
                   Log out
                 </button>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
