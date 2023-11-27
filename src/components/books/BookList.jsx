60eb6fc7165f1324c9e2d482d888e2d6c47d19a2
rafli putra <rafliputra634@gmail.com>
2023-11-27 16:28:29 +0700

    upd : change the book list display for users

--- src/components/books/BookList.jsx
+++ src/components/books/BookList.jsx
@@ -23,89 +23,147 @@ const BookList = () => {
     book.title.toLowerCase().includes(filterBook.toLowerCase())
   );
 
+  // const getDetail = () =>{
+  //   navigate(`/books/detail/${book.id}`)
+  // }
+
   return (
     <div>
       <h1 className="title">Books</h1>
       <h2 className="subtitle">List of Books</h2>
-      <div className="is-flex is-justify-content-space-between">
-        {user && user.role !== "anggota" && (
-          <div className="mb-2">
-            <Link to="/books/add" className="button is-primary">
-              Add New
-            </Link>
-          </div>
-        )}
-
-        <div className="is-flex is-align-items-center mb-2 mr-5">
-          <div className="panel-block">
-            <p className="control has-icons-left">
-              <input
-                className="input" 
-                type="search" 
-                placeholder="Filter by Title"
-                value={filterBook}
-                onChange={(e) => setFilterBook(e.target.value)} 
+      {user && user.role === "anggota" ? (
+        <>
+        <div className="is-flex is-justify-content-space-between">
+          <div className="is-flex is-align-items-center mb-2 mr-5">
+            <div className="panel-block">
+              <p className="control has-icons-left">
+                <input
+                  className="input"
+                  type="search"
+                  placeholder="Filter by Title"
+                  value={filterBook}
+                  onChange={(e) => setFilterBook(e.target.value)}
                 />
-              <span className="icon is-left">
-              <IoSearch/>
-              </span>
-            </p>
+                <span className="icon is-left">
+                  <IoSearch />
+                </span>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <table className="table is-striped is-fullwidth">
-        <thead>
-          <tr>
-            <th>No</th>
-            <th>Cover</th>
-            <th>Title</th>
-            <th>Author</th>
-            {/* <th>Publisher</th> */}
-            <th>Book Status</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredBooks.map((book, index) => (
-            <tr key={book.id}>
-              <td>{index + 1}</td>
-              <td><img src={book.cover} style={{ width: '100px', height: '100px' }} alt="Cover Book" /></td>
-              {/* <td>{imageSrc && <img src={imageSrc} alt="Cover"/>}</td> */}
-              <td>{book.title}</td>
-              <td>{book.author}</td>
-              {/* <td>{book.publisher}</td> */}
-              <td>{book.book_status}</td>
-              <td>
-                <div>
-                  <Link
-                    to={`/books/detail/${book.id}`}
-                    className="button is-small is-info mr-1"
-                  >
-                    Detail
-                  </Link>
-                  {user && user.role !== "anggota" && (
-                    <>
-                      <Link
-                        to={`/books/edit/${book.id}`}
-                        className="button is-small is-warning mr-1"
-                      >
-                        Edit
-                      </Link>
-                      <Link
-                        to={`/books/delete/${book.id}`}
-                        className="button is-small is-danger"
-                      >
-                        Delete
-                      </Link>
-                    </>
-                  )}
+        <div className="columns is-multiline is-2">
+            {filteredBooks.map((book, index) => (
+              <div key={book.id} className="column is-one-fourth">
+              <Link to={`/books/detail/${book.id}`}>
+                <div className="card" style={{ height: '400px', width: '240px'}}>
+                  <div className="card-image is-flex has-text-centered">
+                    <figure className="image" style={{ width: '240px', height: '300px' }}>
+                      <img
+                        src={book.cover}
+                        alt="Cover Book"
+                      />
+                    </figure>
+                  </div>
+                  <div className="card-content">
+                    <p className="title is-6">{book.title}</p>
+                    {/* <p className="subtitle is-6">{book.author}</p>
+                    <p>{book.book_status}</p> */}
+                    </div>
                 </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                </Link>
+              </div>
+            ))}
+          </div>
+          </>
+      ) : (
+        <>
+          <div className="is-flex is-justify-content-space-between">
+            {user && user.role !== "anggota" && (
+              <div className="mb-2">
+                <Link to="/books/add" className="button is-primary">
+                  Add New
+                </Link>
+              </div>
+            )}
+
+            <div className="is-flex is-align-items-center mb-2 mr-5">
+              <div className="panel-block">
+                <p className="control has-icons-left">
+                  <input
+                    className="input"
+                    type="search"
+                    placeholder="Filter by Title"
+                    value={filterBook}
+                    onChange={(e) => setFilterBook(e.target.value)}
+                  />
+                  <span className="icon is-left">
+                    <IoSearch />
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <table className="table is-striped is-fullwidth">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Cover</th>
+                <th>Title</th>
+                <th>Author</th>
+                {/* <th>Publisher</th> */}
+                <th>Book Status</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredBooks.map((book, index) => (
+                <tr key={book.id}>
+                  <td>{index + 1}</td>
+                  <td><img src={book.cover} style={{ width: '80px', height: '100px' }} alt="Cover Book" /></td>
+                  <td>{book.title}</td>
+                  <td>{book.author}</td>
+                  {/* <td>{book.publisher}</td> */}
+                  <td>{book.book_status}</td>
+                  <td>
+                    <div>
+                      <Link
+                        to={`/books/detail/${book.id}`}
+                        className="button is-small is-info mr-1"
+                      >
+                        Detail
+                      </Link>
+                      <Link
+                        to={`/books/borrowing/${book.id}`}
+                        className="button is-small is-success mr-1"
+                      >
+                        Borrow
+                      </Link>
+                      {user && user.role !== "anggota" && (
+                        <>
+                          <Link
+                            to={`/books/edit/${book.id}`}
+                            className="button is-small is-warning mr-1"
+                          >
+                            Edit
+                          </Link>
+                          <Link
+                            to={`/books/delete/${book.id}`}
+                            className="button is-small is-danger"
+                          >
+                            Delete
+                          </Link>
+                        </>
+                      )}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   );
 };
