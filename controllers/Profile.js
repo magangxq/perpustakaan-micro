3efdcc974c9912e30ea13a127e8dcd91c04f117c
baynobu <darmawanhanif30@gmail.com>
2024-02-16 09:53:14 +0700

    editreg

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -1,77 +1,125 @@
 import User from "../models/UserModel.js";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import cloudinary from "../utils/cloudinary.js";
 
-export const Profile = async (req, res) =>{
-    if(!req.session.userId){
-        return res.status(401).json({msg: "Please LogIn first!"});
-    }
-    const user = await User.findOne({
-        attributes:['uuid','name','email','nik_nis', 'registration_status', 'role', 'picture', 'information'],
-        where: {
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User not found"});
-    res.status(200).json(user);
-}
+export const Profile = async (req, res) => {
+  if (!req.session.userId) {
+    return res.status(401).json({ msg: "Please LogIn first!" });
+  }
+  const user = await User.findOne({
+    attributes: [
+      "uuid",
+      "name",
+      "email",
+      "nik_nis",
+      "registration_status",
+      "role",
+      "picture",
+      "information",
+    ],
+    where: {
+      uuid: req.session.userId,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  res.status(200).json(user);
+};
 
-export const editProfile = async(req, res) =>{
-    const user = await User.findOne({
+export const editRegistration = async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      uuid: req.params.id,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  const { name, email, nik_nis } = req.body;
+  try {
+    await User.update(
+      {
+        name: name,
+        email: email,
+        nik_nis: nik_nis,
+        registration_status: "waiting-verification",
+        information: null, // I assumed you want to set it to an empty string
+      },
+      {
         where: {
-            uuid: req.params.id
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User not found"});
-    const {name} = req.body;
-    try {
-        await User.update({
-            name: name
-        },{
-            where:{
-                id: user.id
-            }
-        });
-        res.status(200).json({msg: "Name Updated"});
-    } catch (error) {
-        res.status(400).json({msg: error.message});
-    }
-}
+          id: user.id,
+        },
+      }
+    );
+
+    res.status(200).json({ msg: "Updated" });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
+export const editProfile = async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      uuid: req.params.id,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "User not found" });
+  const { name } = req.body;
+  try {
+    await User.update(
+      {
+        name: name,
+      },
+      {
+        where: {
+          id: user.id,
+        },
+      }
+    );
+    res.status(200).json({ msg: "Name Updated" });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
 
 export const uploadImage = async (req, res) => {
-    try {
-      const userUuid = req.session.userId;
-  
-      // Find the user based on the session UUID
-      const user = await User.findOne({
-        where: { uuid: userUuid },
-      });
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-  
-      cloudinary.uploader.upload(req.file.path, { folder: `project-perpus/users-picture` }, async function (err, result) {
+  try {
+    const userUuid = req.session.userId;
+
+    // Find the user based on the session UUID
+    const user = await User.findOne({
+      where: { uuid: userUuid },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    cloudinary.uploader.upload(
+      req.file.path,
+      { folder: `project-perpus/users-picture` },
+      async function (err, result) {
         if (err) {
           console.log(err);
           return res.status(500).json({
             success: false,
-            message: err
+            message: err,
           });
         }
-  
+
         user.picture = result.secure_url;
         await user.save();
-  
+
         return res.json({
           success: true,
-          message: 'Success upload profile picture.',
-          data: result
+          message: "Success upload profile picture.",
+          data: result,
         });
-      });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'An error occurred while uploading the profile picture.', error });
-    }
-  };
-  
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: "An error occurred while uploading the profile picture.",
+      error,
+    });
+  }
+};
--- routes/ProfileRoute.js
+++ routes/ProfileRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {Profile, editProfile, uploadImage} from "../controllers/Profile.js";
+import {Profile, editProfile, uploadImage, editRegistration} from "../controllers/Profile.js";
 import { regStatus, verifyUser } from "../middleware/AuthUser.js";
 import { upload } from "../middleware/multer.js";
 
@@ -9,5 +9,6 @@ const router = express.Router();
 router.get('/profile', verifyUser, Profile);
 router.patch('/edit-profile/:id', verifyUser, regStatus, editProfile);
 router.post('/upload-picture', verifyUser, regStatus,upload.single('profilePicture'), uploadImage);
+router.patch('/edit-registration/:id', verifyUser, editRegistration);
 
 export default router;
